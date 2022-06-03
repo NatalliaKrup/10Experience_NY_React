7c88e2511a1036ba4e9f7d1cd6e9b95a98c376eb
Natallia <natalliakrup@gmail.com>
2022-06-03 11:36:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import logo from "./logo.png"
+import bin from "./bin.png"
+import likebtn from "./like.png"
+import winkelrechts from "./winkelrechts.png"
+import winkellinks from "./winkellinks.png"
+import { data } from './data';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [sightNY, setSightNY] = useState(data)
+
+								// showMore
+	const[showMoreText, setShowMoreText] = useState(false)
+	const btnMoreText =(item)=>{
+		item.showMore = !item.showMore
+		setShowMoreText(!showMoreText)
+	}
+								// remove one Item
+	const removeItem =(id)=>{
+	const newSightNY = sightNY.filter(item =>item.id !== id)
+	setSightNY(newSightNY)
+	}
+								// next slide
+	const [t, setT] = useState(0);
+	const [datas, setDatas] = useState(data);
+	let newArr = [...datas]
+	
+	const nextFoto =(e, index)=>{
+		const slide = document.querySelectorAll("#slide");
+		newArr [index]= sightNY[index];
+		setDatas(newArr);
+
+		setT ((t => {
+			t++;
+			if (t>3){
+				t=0
+			}
+			return t;
+		}))
+		slide[index].setAttribute("src", newArr[index].image[t]);
+		return slide;
+	}
+
+	const previosFoto =(e, index)=>{
+		const slide = document.querySelectorAll("#slide");
+		newArr [index]= sightNY[index];
+		setDatas(newArr);
+
+		setT ((t =>{
+			t--;
+			if (t<0){
+				t=3
+			}
+			return t;
+		}))
+		slide[index].setAttribute("src", newArr[index].image[t]);
+		return slide;
+	}
+	
+									// Likes
+	const [likeCount, setLikeCount] = useState([0,0,0,0,0,0,0,0,0,0])
+	const updateLikes=(e, index)=>{
+		const likeHolder = document.querySelectorAll("#likeHolder");
+		setLikeCount ((count => {
+			likeCount[index]++;
+			return likeCount;
+		}))
+		likeHolder[index].innerHTML = (sightNY[index].like) + likeCount[index];
+		return likeHolder
+	}
+
+									// reload
+	const reloadPage =()=>{
+		window.location.reload()
+	}
+
+	return (
+		<div className="app">
+			<div className='container'>
+				<img className='logo' src={logo}/>
+				<h1>New York </h1>
+				<h2>Top {data.length} unmissable Experiences</h2>
+					{ sightNY.map((element =>{
+						const{id, nameSight, description, showMore, image, address,googlemap, like} = element;
+						const index = sightNY.indexOf(element)
+						return(
+							<div className='container_flex' key={id}>
+								<div className='flex_description'>
+									<h3>{id}: {nameSight}</h3>
+									<p> {showMore? description: description.substring(0,220) + "... "}
+										<button onClick={()=>btnMoreText(element)}>{showMore? "show less": "show more"}</button>
+									</p>
+									<h5>Addresse: <a href={googlemap} target="_blank" rel="noreferrer"><span>{address}</span></a></h5>
+								</div>
+
+								<div className='flex_image'>
+									<button onClick={(e)=>previosFoto(e, index)}>
+										<img className='scroll scroll_back' src={winkellinks} alt="next"/>
+									</button>
+
+									<div className='scale'>
+										<img className='scale' id='slide' src={image[0]} alt="NewYork"/>
+									</div>
+
+									<button onClick={(e)=>nextFoto(e, index)}>
+										<img className='scroll scroll_next' src={winkelrechts} alt="back"/>
+									</button>
+								</div>
+
+								<div className='flex_rating'>
+									<div className='rating_like'>
+										<p id="likeHolder" value="">{like}</p>
+										<button onClick={(e)=>updateLikes(e, index)}>
+											<img src={likebtn} width="20" alt="like"/>
+										</button>
+									</div>
+									<div className='raiting_bin'>
+										<button onClick={()=>removeItem(id)} >
+											<img src={bin} width="20" alt="bin"/>
+										</button>
+									</div>
+								</div>
+							
+							</div>
+						)
+					}))
+				}
+				<div className='container_footer'>
+					<button onClick={()=>reloadPage()}>Reload Page</button>
+					<p>Information was taken from the website: <a href='https://www.introducingnewyork.com/top-10'>civitatis New York</a> and <a href='https://www.attractionsofamerica.com/attractions/new-york-city-top-10-attractions.php'>attractiond of Americas</a></p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,158 @@
+import statue1 from "./statue1.jpg"
+import statue2 from "./statue2.jpg"
+import statue3 from "./statue3.jpg"
+import statue4 from "./statue4.jpg"
+
+import park1 from "./park1.jpg"
+import park2 from "./park2.jpg"
+import park3 from "./park3.jpg"
+import park4 from "./park4.jpg"
+
+import square1 from "./square2.jpg"
+import square2 from "./square1.jpg"
+import square3 from "./square3.jpg"
+import square4 from "./square4.jpg"
+
+import bridge1 from "./bridge1.jpg"
+import bridge2 from "./bridge2.jpg"
+import bridge3 from "./bridge3.jpg"
+import bridge4 from "./bridge4.jpg"
+
+import empire1 from "./empire1.jpg"
+import empire2 from "./empire2.jpg"
+import empire3 from "./empire3.jpg"
+import empire4 from "./empire4.jpg"
+
+import fifth1 from "./fifth1.jpg"
+import fifth2 from "./fifth2.jpg"
+import fifth3 from "./fifth3.jpg"
+import fifth4 from "./fifth4.jpg"
+
+import rockefeller1 from "./rockefeller1.jpg"
+import rockefeller2 from "./rockefeller2.jpg"
+import rockefeller3 from "./rockefeller3.jpg"
+import rockefeller4 from "./rockefeller4.jpg"
+
+import museum1 from "./museum1.jpeg"
+import museum2 from "./museum2.jpg"
+import museum3 from "./museum3.jpg"
+import museum4 from "./museum4.jpg"
+
+import natural1 from "./natural1.jpg"
+import natural2 from "./natural2.jpeg"
+import natural3 from "./natural3.jpg"
+import natural4 from "./natural4.jpg"
+
+import cathedral1 from "./cathedral1.jpg"
+import cathedral2 from "./cathedral2.jpg"
+import cathedral3 from "./cathedral3.jpg"
+import cathedral4 from "./cathedral4.jpg"
+
+
+export const data =[
+	{
+		id:1,
+		nameSight:"Statue of Liberty",
+		description:"There's no denying that the Statue of Liberty is one of the most iconic landmarks in the US. Built in 1886 as a gift to America from France, it's almost synonymous with America. It's also one of the biggest statues in the world, weighing 450,000 pounds and measuring nearly 152 feet in height.You have to take a boat or ferry to visit the Statue of Liberty up close to Liberty Island. Most people come from Battery Park, and you'll have to get in line pretty early for a smooth experience and ticket pre-purchases are highly recommended. There are also helicopter tours of New York City that will fly over the statue for a bird's-eye view of the stunning green landmark.",
+		showMore: false,
+		image:[statue1, statue2, statue3, statue4 ],
+		address:"Liberty Island, New York City, NY 10004",
+		googlemap: "https://www.google.com/maps/place/%D0%9E%D1%81%D1%82%D1%80%D0%BE%D0%B2+%D0%9B%D0%B8%D0%B1%D0%B5%D1%80%D1%82%D0%B8/@40.6898413,-74.0497357,16z/data=!3m1!4b1!4m5!3m4!1s0x89c2508ed1e8d553:0x79084fab31e7ed6c!8m2!3d40.6895453!4d-74.0449292",
+		like:200,
+	},
+
+	{
+		id: 2,
+		nameSight: "Central Park",
+		description:"There's simply no way to vacation in New York City without heading to Central Park. This is a gigantic man-made haven spanning 843 acres, with hillsides, gardens, forests, and 58 miles of pathways. Sculptures, bridges, artworks, monuments, fountains, playgrounds, tunnels, and plenty of facilities dot the landscape, giving you endless things to do. It's no surprise that this green space is one of the top attractions in New York City! It's not possible to get into every single one of the individual features of Central Park. There are a few notable spots, of course. There's the seasonal pond of Conservatory Water, the romantic Bethesda Fountain topped by a sculpture of the Angel of Water, and the Loeb Boathouse. There's also the Strawberry Fields, the Wollman Memorial Rink, and the Jacqueline Kennedy Onassis Reservoir. And don't forget the 11-foot Alice in Wonderland statue! Special events, concerts, and other festivals often occur at Central Park, so check the city's local events to see if there's anything extra going on at the park while you're there. The chances are that there'll be something interesting happening!",
+		showMore: false,
+		image:[park1, park2, park3, park4],
+		address: "14 E 60th St, Central Park Conservancy, New York City, NY 10022",
+		googlemap: "https://www.google.com/maps/place/%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9+%D0%BF%D0%B0%D1%80%D0%BA/@40.7812199,-73.9665138,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2589a018531e3:0xb9df1f7387a94119!8m2!3d40.7812199!4d-73.9665138",
+		like: 300,
+	},
+
+			{
+		id:3,
+		nameSight:"Times square",
+		description:"It's hard to consider what to do in New York City without considering Times Square. Many people think this five-block expanse to be almost synonymous with the city itself, congested and high priced as a commercial tourist trap. There's nothing quite as simultaneously overwhelming and awe-inspiring as standing in Times Square and watching the world go by around you. The bright screens and billboards add even more to the visual noise. Hang out here at night and people watch or just get lost in the lights!",
+		showMore:false,
+		image:[square1, square2, square3, square4],
+		address:"Manhattan, NY 10036",
+		googlemap:"https://www.google.ru/maps/place/Times+Square,+New+York,+NY+10036,+USA/@40.7580033,-73.9866631,18z/data=!3m1!4b1!4m5!3m4!1s0x89c25855a96da09d:0x860bf5a5e1a00a68!8m2!3d40.7579747!4d-73.9855426",
+		like: 612,
+	},
+
+		{
+		id:4,
+		nameSight:"Brooklyn Bridge",
+		description:"Brooklyn Bridge is one of the most iconic landmark sites in New York City. It's also one of America's oldest suspension bridges and the very first steel suspension bridge on the planet. The 1883 creation is known for its Gothic arches and thick, strong suspension cables that support its six traffic lanes. The Brooklyn Bridge runs from Brooklyn to Manhattan, crossing the East River. A walkway for pedestrians has facilitated many tourist walks over the years as people travel across the bridge on foot or bike. The 1,595-foot stretch is suitable for a pleasant excursion and is entirely free, though guided walks are also offered at a price!",
+		showMore:false,
+		image:[bridge1, bridge2, bridge3, bridge4],
+		address:"Brooklyn Bridge, New York City, NY 10038",
+		googlemap:"https://www.google.ru/maps/place/Brooklyn+Bridge/@40.7058094,-73.9981676,17z/data=!3m1!4b1!4m5!3m4!1s0x89c25a2343ce7b2b:0x2526ddba7abd465c!8m2!3d40.7060855!4d-73.9968643",
+		like:490,
+	},
+
+		{
+		id:5,
+		nameSight:"Empire State Building",
+		description:"The Empire State Building is one of the most iconic and beloved New York City tourist attractions. The stunning art deco creation is nothing short of spectacular, with a modern but dynamic lighting system boasting 16 million potential colors. Light shows are common during special events and holidays. Trips to the top of the Empire State Building are especially popular and are practically a must-do. The fees are high and the lines are long, but it's an ordeal very much worth the effort, so you can see all of the city below from a 1,050-foot height. There's also a multimedia tour of the building that helps guide you through the various views and exhibits that the building has to offer.",
+		showMore: false,
+		image:[empire1, empire2, empire3, empire4],
+		address:" 20 W 34th St, New York City, NY 10001",
+		googlemap:"https://www.google.ru/maps/place/20+W+34th+St,+New+York,+NY+10118,+USA/@40.7487698,-73.9879135,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259a90897ddaf:0x5fd95bb4ffa32d23!8m2!3d40.7487658!4d-73.9857248",
+		like: 113,
+	},
+			{
+		id:6,
+		nameSight:"Fifth Avenue",
+		description:"It's simply not possible to discuss the places to go in New York City without mentioning Fifth Avenue. It's packed with high-end shopping and historic buildings one after the other. Whether you're a big spender or a browser, you'll enjoy the retail offerings and well-known businesses that populate the location. Flagship stores, trendy designers, and luxurious products of all sorts line Fifth Avenue. You'll find stores for Apple, Cartier, Bergdorf-Goodman, Saks, Tiffany, Prada, and much more. Many of these shops are also the most popular of their respective brands, even considered famous on a global scale.",
+		showMore:false,
+		image:[fifth1, fifth2, fifth3, fifth4],
+		address:"7 Best Shopping Malls in New York City",
+		googlemap:"https://www.google.ru/maps/search/7+Best+Shopping+Malls+in+New+York+City/@40.7487898,-73.9944796,15z/data=!3m1!4b1",
+		like: 356,
+	},
+		{
+		id:7,
+		nameSight:"Rockefeller Center",
+		description:"The Rockefeller Center is one of the most iconic NYC attractions, boasting a fishbowl glimpse into the studios of NBC and decorated with awe-inspiring sculptures. There's a huge skating rink, several restaurants and stores, and much more to enjoy at this entertainment complex. There's an additional draw of the central plaza: the Top of the Rock Observation Deck. Here, you'll get some of the very best views in all of Manhattan atop a 70-story skyscraper that serves as the centerpiece of the Rockefeller Center. The deck takes up a total of three floors, providing jaw-dropping views of the world around you no matter what time of the day it is!",
+		showMore:false,
+		image:[rockefeller1, rockefeller2, rockefeller3, rockefeller4],
+		address:"45 Rockefeller Plaza, New York City, NY 10111",
+		googlemap:"https://www.google.ru/maps/place/45+Rockefeller+Plaza,+New+York,+NY+10020,+USA/@40.759628,-73.9809217,17z/data=!3m1!4b1!4m5!3m4!1s0x89c258f9357dd5c5:0xfaf575f5e2145d99!8m2!3d40.759624!4d-73.978733",
+		like:578,
+	},
+			{
+		id:8,
+		nameSight:"The Metropolitan Museum of Art",
+		description:"There are only a few more famous and revered museums across America than the Metropolitan Museum of Art. It's not just renowned in New York City but across the planet too, often referred to affectionately as The Met. The vast permanent collection covers 5,000 years of art history and is pretty much impossible to see in its entirety in one day. There's so much to see at The Met that, like Central Park, it's impossible to cover every single interesting facet. There are works by great European masters, textiles from Asian countries, sculptures from ancient Rome and Greece, and clothing made by famous designers. There's an authentic Egyptian temple dating back 2,000 years, armor and weapons from around the world, photographs, musical instruments, and beyond. Rotating exhibits ensure that The Met is always fresh, even with all its already existing variety.",
+		showMore:false,
+		image:[museum1, museum2, museum3, museum4],
+		address: "1000 5th Ave, New York City, NY 10028",
+		googlemap:"https://www.google.ru/maps/place/1000+5th+Ave,+New+York,+NY+10028,+USA/@40.7791862,-73.9722681,15z/data=!3m1!4b1!4m5!3m4!1s0x89c25896dcda965d:0x5bbcfab18ea2c739!8m2!3d40.7791865!4d-73.9635348",
+		like:97,
+	},
+			{
+		id:9,
+		nameSight:" American Museum of Natural History",
+		description:"The American Museum of Natural History is situated close to Central Park West. It's one of the most crucial New York City attractions for history buffs, teaching you all about space, land, sea, history, and more across four city blocks, 32 million artifacts, 45 halls, and 25 buildings.  In a similar theme to The Met and Central Park, there is simply so much to do at the American Museum of Natural History that it's impossible to cover every one of its facets. Notable stops are the Hall of the Universe, the dinosaur skeletons, the Rose Center for Earth and Space, the Butterfly Conservatory, the Milstein Hall of Ocean Life, and the unique Asiatic Leopard Diorama.",
+		showMore: false,
+		image: [natural1, natural2, natural3, natural4],
+		address:"200 Central Park West, New York City, NY 10024",
+		googlemap:"https://www.google.ru/maps/place/200+Central+Park+West,+New+York,+NY+10024,+USA/@40.7812694,-73.9760088,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2588f006b028f:0xeafe921ed8b8cdb!8m2!3d40.7812654!4d-73.9738201",
+		like: 219,
+	},
+
+		{
+		id: 10,
+		nameSight:"St. Patrick's Cathedral",
+		description:"St. Patrick's Cathedral is one of the very best attractions in New York City for Gothic Revival architecture. Boasting spires that reach heights of 330 feet, a grandiose set of bronze doors, and a regal white marble facade, this is a marvel you just have to see! You don't need to be religious to appreciate the decorated neo-Gothic beauty of St. Patrick's Cathedral. Inside, you'll see a jaw-dropping stained-glass rose window, enough seats for 2,400 members of the congregation, a stunning Great Organ, and a bronze baldachin.",
+		showMore: false,
+		image:[cathedral1, cathedral2, cathedral3, cathedral4],
+		address:"5th Ave, New York City, NY 10022",
+		googlemap:"https://www.google.ru/maps/place/5th+Ave,+New+York,+NY+10022,+USA/@40.7612003,-73.9774523,17z/data=!3m1!4b1!4m5!3m4!1s0x89c258a29d3847f5:0x564dfbba0141774a!8m2!3d40.7611963!4d-73.9752636",
+		like: 366,
+	}
+]
